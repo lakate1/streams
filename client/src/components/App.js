b8f28d687a0d2eea5f14dac8fb619482466f2001
Leah Queen <leah836@gmail.com>
2020-12-22 12:22:13 -0700

    Added links to routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,24 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamDelete from "./streams/StreamDelete";
 import StreamEdit from "./streams/StreamEdit";
 import StreamList from "./streams/StreamList";
 import StreamShow from "./streams/StreamShow";
+import Header from "./Header";
+import 'semantic-ui-css/semantic.min.css';
 
 const App = () => {
     return (
-    <div>
+    <div className="ui container">
         <BrowserRouter>
-        <div>
+         <div>
+            <Header />
             <Route path="/" exact component={StreamList} />
             <Route path="/streams/new" exact component={StreamCreate} />
             <Route path="/streams/edit" exact component={StreamEdit} />
             <Route path="/streams/delete" exact component={StreamDelete} />
             <Route path="/streams/show" exact component={StreamShow} />
-
             </div>
         </BrowserRouter>
     </div>
